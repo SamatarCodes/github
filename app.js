91ffcd60bb2a147684b504dc0132c8b31951800c
SamatarCodes <thewind07@protonmail.com>
2020-06-23 22:35:04 -0400

    added eventlistener to search input

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const header = document.querySelector('.header');
 const input = document.querySelector('.nav-toggle');
+const searchInput = document.querySelector('#search-input');
 
+// * This is for the dropdown menu toggle
 input.addEventListener('change', (e) => {
   if (e.target.checked) {
     header.classList.add('addToHeader');
@@ -8,3 +10,11 @@ input.addEventListener('change', (e) => {
     header.classList.remove('addToHeader');
   }
 });
+
+// Search input
+searchInput.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
+});
+
+// Create a helper function
